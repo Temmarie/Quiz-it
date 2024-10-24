fac4332c9c5033f2a4c5fdbad18841251007f759
Temmarie <temmar_whyte16@yahoo.com>
2024-10-24 09:45:45 +0100

    Fix user authentication and add access restriction

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { SignedIn, SignedOut, RedirectToSignIn } from '@clerk/clerk-react';
 import Dashboard from './components/Dashboard';
 import Profile from './components/Profile';
 import SignInPage from './components/SignIn';
@@ -8,18 +9,65 @@ import HomePage from './components/HomePage';
 import Layout from './components/Layout';
 import './index.css'
 
-const App = () => (
+const App = () => {
+  return (
     <Router>
-        <Layout>
-            <Routes>
-                <Route path="/" element={<HomePage />} /> 
-                <Route path="/dashboard" element={<Dashboard />} />
-                <Route path="/profile" element={<Profile />} />
-                <Route path="/sign-in" element={<SignInPage />} />
-                <Route path="/sign-up" element={<SignUpPage />} />
-            </Routes>
-       </Layout>
+      <Layout>
+        <Routes>
+          {/* HomePage Route */}
+          <Route
+            path="/"
+            element={<HomePage />}
+          />
+          {/* Dashboard Route */}
+          <Route
+            path="/dashboard"
+            element={
+              <SignedIn>
+                <Dashboard />
+              </SignedIn>
+            }
+          />
+          {/* Profile Route */}
+          <Route
+            path="/profile"
+            element={
+              <SignedIn>
+                <Profile />
+              </SignedIn>
+            }
+          />
+          {/* Sign In Route */}
+          <Route
+            path="/sign-in"
+            element={
+              <SignedOut>
+                <SignInPage />
+              </SignedOut>
+            }
+          />
+          {/* Sign Up Route */}
+          <Route
+            path="/sign-up"
+            element={
+              <SignedOut>
+                <SignUpPage />
+              </SignedOut>
+            }
+          />
+          {/* Redirect unauthenticated user to sign-in */}
+          <Route
+            path="*"
+            element={
+              <SignedOut>
+                <RedirectToSignIn />
+              </SignedOut>
+            }
+          />
+        </Routes>
+      </Layout>
     </Router>
-);
+  );
+};
 
 export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,54 +3,60 @@ import { Link } from 'react-router-dom';
 import { useUser, useClerk } from '@clerk/clerk-react'
 
 const Header = () => {
-    const { user } = useUser();
-    const { signOut } = useClerk();
-  
-    return (
-      <nav className="flex items-center justify-between px-6 py-4 mb-5 bg-black">
-        <div className="flex items-center">
-          <Link to="/">
-            <div className="text-lg font-bold text-purple-300 uppercase">
-              ClerkApp
-            </div>
-          </Link>
-        </div>
-        <div className="flex items-center text-purple-200">
-          {!user && (
-            <>
-              <Link
-                to="/sign-in"
-                className="text-purple-300 hover:text-purple-400 mr-4"
-              >
-                Sign In
-              </Link>
-              <Link
-                to="/sign-up"
-                className="text-purple-300 hover:text-purple-400 mr-4"
-              >
-                Sign Up
-              </Link>
-            </>
-          )}
-          {user && (
-            <>
-              <Link
-                to="/profile"
-                className="text-purple-300 hover:text-purple-400 mr-4"
-              >
-                Profile
-              </Link>
-              <button
-                onClick={() => signOut()}
-                className="text-purple-300 hover:text-purple-400"
-              >
-                Sign Out
-              </button>
-            </>
-          )}
-        </div>
-      </nav>
-    );
-  };
-  
-  export default Header;
+  const { user } = useUser();
+  const { signOut } = useClerk();
+
+  return (
+    <nav className="flex items-center justify-between px-6 py-4 mb-5 bg-black">
+      <div className="flex items-center">
+        <Link to="/">
+          <div className="text-lg font-bold text-purple-300 uppercase">
+            ClerkApp
+          </div>
+        </Link>
+      </div>
+      <div className="flex items-center text-purple-200">
+        {!user && (
+          <>
+            <Link
+              to="/sign-in"
+              className="text-purple-300 hover:text-purple-400 mr-4 uppercase"
+            >
+              Sign In
+            </Link>
+            <Link
+              to="/sign-up"
+              className="text-purple-300 hover:text-purple-400 mr-4 uppercase"
+            >
+              Sign Up
+            </Link>
+          </>
+        )}
+        {user && (
+          <>
+            <Link
+              to="/dashboard"
+              className="text-purple-300 hover:text-purple-400 mr-4"
+            >
+              Dashboard
+            </Link>
+            <Link
+              to="/profile"
+              className="text-purple-300 hover:text-purple-400 mr-4"
+            >
+              Profile
+            </Link>
+            <button
+              onClick={() => signOut()}
+              className="text-purple-300 hover:text-purple-400"
+            >
+              Sign Out
+            </button>
+          </>
+        )}
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
