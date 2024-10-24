7e00c377a3818462ca5e2ed51fc152c9d69f2d1f
Temmarie <temmar_whyte16@yahoo.com>
2024-10-24 09:16:40 +0100

    Set up authentication components

---
+++ src/components/SignIn.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { SignIn } from '@clerk/clerk-react';
+
+const SignInPage = () => (
+    <div className="flex items-center justify-center min-h-screen">
+      <div className="text-center">
+        <SignIn path="/sign-in" routing="path" />
+      </div>
+    </div>
+  );
+  
+  export default SignInPage;
---
+++ src/components/SignUp.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { SignUp } from '@clerk/clerk-react';
+
+const SignUpPage = () => (
+  <div className="flex items-center justify-center min-h-screen">
+    <div className="text-center">
+      <SignUp path="/sign-up" routing="path" />
+    </div>
+  </div>
+);
+
+export default SignUpPage;
