47cfb7135fa4697609e89298706b438994429fe8
Temmarie <temmar_whyte16@yahoo.com>
2024-10-24 08:38:27 +0100

    Add Clerk credentials to .env file and wrap App with Clerk Provider

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client'
+import App from './App';
+import './index.css';
+import { ClerkProvider } from '@clerk/clerk-react'; 
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error('Clerk publishable key is missing.');
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
     <App />
-  </StrictMode>,
-)
+  </ClerkProvider>
+);
