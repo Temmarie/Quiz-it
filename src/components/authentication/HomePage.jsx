64dd2378daefaf93f1994990cc09f35596781d33
Temmarie <temmar_whyte16@yahoo.com>
2025-01-16 23:09:25 +0100

    Add new routing to homepage

--- src/components/authentication/HomePage.jsx
+++ src/components/authentication/HomePage.jsx
@@ -1,16 +1,23 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
-const HomePage = ({  startQuiz ,showInstructionsScreen }) => {
+const HomePage = () => {
+  const navigate = useNavigate();
+
+  const handleStartQuiz = () => {
+    navigate('/instructions'); // Navigate to instructions page
+  };
+
   return (
-      <div className="flex flex-col items-center justify-center h-screen bg-white text-indigo-600">
-          <h2 className="text-4xl font-bold mb-4">Welcome to the Quiz!</h2>
-
-          <button className="bg-indigo-500 text-white px-6 py-3 rounded-lg mt-4  mb-4" onClick={showInstructionsScreen} aria-label="Show Instructions" >
-            Show Instructions </button>
-
-          <button className="bg-indigo-500 text-white px-6 py-3 rounded-lg" onClick={startQuiz}  aria-label="Start Quiz">
-              Start Quiz </button>
-      </div>
+    <div className="flex flex-col items-center justify-center h-screen bg-white text-indigo-600">
+      <h2 className="text-4xl font-bold mb-4">Welcome to the Quiz!</h2>
+      <button
+        className="mt-6 bg-indigo-500 text-white px-4 py-2 rounded-lg"
+        onClick={handleStartQuiz}
+      >
+        Start Quiz
+      </button>
+    </div>
   );
 };
 
