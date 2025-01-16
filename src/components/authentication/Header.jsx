73c18a0777c5fd825947d122c702a6106c2b0d22
Temmarie <temmar_whyte16@yahoo.com>
2025-01-16 23:08:08 +0100

    Change color scheme for Header

--- src/components/authentication/Header.jsx
+++ src/components/authentication/Header.jsx
@@ -7,26 +7,26 @@ const Header = () => {
   const { signOut } = useClerk();
 
   return (
-    <nav className="flex items-center justify-between px-6 py-4 mb-5 bg-black">
+    <nav className="flex items-center justify-between px-6 py-4 mb-5 bg-indigo-950">
       <div className="flex items-center">
         <Link to="/">
-          <div className="text-lg font-bold text-purple-300 uppercase">
+          <div className="text-lg font-bold text-indigo-400 uppercase">
             QUIZ IT
           </div>
         </Link>
       </div>
-      <div className="flex items-center text-purple-200">
+      <div className="flex items-center text-indigo-300">
         {!user && (
           <>
             <Link
               to="/sign-in"
-              className="text-purple-300 hover:text-purple-400 mr-4 uppercase"
+              className=" hover:text-indigo-400 mr-4 uppercase"
             >
               Sign In
             </Link>
             <Link
               to="/sign-up"
-              className="text-purple-300 hover:text-purple-400 mr-4 uppercase"
+              className=" hover:text-indigo-400 mr-4 uppercase"
             >
               Sign Up
             </Link>
@@ -36,19 +36,19 @@ const Header = () => {
           <>
             <Link
               to="/dashboard"
-              className="text-purple-300 hover:text-purple-400 mr-4"
+              className=" hover:text-indigo-400 mr-4"
             >
               Dashboard
             </Link>
             <Link
               to="/profile"
-              className="text-purple-300 hover:text-purple-400 mr-4"
+              className=" hover:text-indigo-400 mr-4"
             >
               Profile
             </Link>
             <button
               onClick={() => signOut()}
-              className="text-purple-300 hover:text-purple-400"
+              className=" hover:text-indigo-400"
             >
               Sign Out
             </button>
