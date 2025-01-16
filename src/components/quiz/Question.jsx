3ccb2fcb5de612747e32d29f70b79aaf9fdc1f48
Temmarie <temmar_whyte16@yahoo.com>
2025-01-16 23:12:16 +0100

    Add new features to questons component

---
+++ src/components/quiz/Question.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+function Questions({ question, handleAnswer, questionNumber }) {
+  return (
+    <div>
+      <h2 className="text-indigo-950 font-semibold mb-3">
+         {questionNumber}: {question.question}
+      </h2>
+      <div className="options flex flex-col justify-evenly mb-2">
+        {question.options.map((option, index) => (
+          <button
+            key={index}
+            onClick={() => handleAnswer(index === question.correctIndex)}
+            className="bg-indigo-500 text-white px-3 py-2 rounded-lg mb-3"
+          >
+            {option}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Questions;
